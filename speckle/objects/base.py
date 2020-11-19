d01d7824bc87e3b6783b3ae38b9864cfbd6327a7
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-11-19 16:02:32 +0000

    feat(base): get total children count

--- speckle/objects/base.py
+++ speckle/objects/base.py
@@ -30,5 +30,49 @@ class Base(BaseModel):
         """Get all of the names of the dynamic properties of this object"""
         return list(set(self.__dict__.keys()) - set(self.__fields__.keys()))
 
+    def get_children_count(self) -> int:
+        """Get the total count of children Base objects"""
+        parsed = []
+        return 1 + self._count_descendants(self, parsed)
+
+    def _count_descendants(self, base: Base, parsed: List) -> int:
+        if base in parsed:
+            return 0
+        parsed.append(base)
+
+        count = 0
+
+        for name, value in base.__dict__.items():
+            if name.startswith("@"):
+                continue
+            else:
+                count += self._handle_object_count(value, parsed)
+
+        return count
+
+    def _handle_object_count(self, obj: Any, parsed: List) -> int:
+        count = 0
+        if obj == None:
+            return count
+        if isinstance(obj, Base):
+            count += 1
+            count += self._count_descendants(obj, parsed)
+            return count
+        elif isinstance(obj, list):
+            for item in obj:
+                if isinstance(item, Base):
+                    count += 1
+                    count += self._count_descendants(item, parsed)
+                else:
+                    count += self._handle_object_count(item, parsed)
+        elif isinstance(obj, dict):
+            for _, value in obj.items():
+                if isinstance(value, Base):
+                    count += 1
+                    count += self._count_descendants(value, parsed)
+                else:
+                    count += self._handle_object_count(value, parsed)
+        return count
+
     class Config:
         extra = Extra.allow
