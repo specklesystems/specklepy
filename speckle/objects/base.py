9a27ed1544e994e1df5290a9bb22f9c1e5674613
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-11-19 09:24:13 +0000

    feat(base): start `Base` object with dict set/get

---
+++ speckle/objects/base.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from typing import List, Optional, Any
+
+from pydantic import BaseModel
+from pydantic.main import Extra
+
+
+class Base(BaseModel):
+    id: Optional[str] = None
+    totalChildrenCount: Optional[int] = None
+    applicationId: Optional[str] = None
+    speckle_type: Optional[str] = None
+
+    def __setitem__(self, name: str, value: Any) -> None:
+        self.__dict__[name] = value
+
+    def __getitem__(self, name: str) -> Any:
+        return self.__dict__[name]
+
+    def get_member_names(self) -> List[str]:
+        """Get all of the property names on this object, dynamic or not"""
+        return list(self.__dict__.keys())
+
+    def get_typed_member_names(self) -> List[str]:
+        """Get all of the names of the defined (typed) properties of this object"""
+        return list(self.__fields__.keys())
+
+    def get_dynamic_member_names(self) -> List[str]:
+        """Get all of the names of the dynamic properties of this object"""
+        props = self.get_member_names()
+        typed = self.get_typed_member_names()
+        return [prop for prop in props if prop not in typed]
+
+    class Config:
+        extra = Extra.allow
