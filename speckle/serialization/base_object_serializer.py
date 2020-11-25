37480b1c9e8b45ddbfc461b47e1594b569807a9f
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-11-25 22:04:48 +0000

    feat(serialization): create closure table

--- speckle/serialization/base_object_serializer.py
+++ speckle/serialization/base_object_serializer.py
@@ -10,6 +10,10 @@ def hash_dict(obj: dict) -> str:
 
 
 class BaseObjectSerializer:
+    detach_lineage: list = []
+    closure_table: dict = {}
+    traversed_objects: dict = {}
+
     def __init__(self) -> None:
         pass
 
@@ -17,6 +21,8 @@ class BaseObjectSerializer:
         raise NotImplementedError
 
     def traverse_base(self, base: Base) -> Tuple[str, dict]:
+        if not self.detach_lineage:
+            self.detach_lineage.append(False)
         stack = [(base, base.get_member_names())]
         object_dict = {}
         while stack:
@@ -27,21 +33,26 @@ class BaseObjectSerializer:
                 if not value or prop.startswith("__"):
                     continue
                 if isinstance(value, Base):
-                    ref_hash, child_obj = self.traverse_base(value)
                     if prop.startswith(("@", "__")):
+                        self.detach_lineage.append(True)
+                        ref_hash, _ = self.traverse_base(value)
                         object_dict[prop] = {
                             "reference_id": ref_hash,
                             "speckle_type": "reference",
                         }
+                        self.closure_table[ref_hash] = len(self.detach_lineage) + 1
                     else:
+                        self.detach_lineage.append(False)
+                        _, child_obj = self.traverse_base(value)
                         object_dict[prop] = child_obj
                     stack.extend([(obj, props)])
                     break
                 object_dict[prop] = value
         hash = hash_dict(object_dict)
         object_dict["id"] = hash
+        self.traversed_objects[hash] = object_dict
 
-        # print(f"==== traversed base {base.name} ====")
-        # print(json.dumps(object_dict, indent=2))
+        if self.detach_lineage:
+            self.detach_lineage.pop()
 
         return hash, object_dict
