eee726e252193f8111a43688621294f9b7604913
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-11-26 18:25:44 +0000

    feat(serialisation): closures & handle iterables
    - add '__closures' to each object
    - construct the full closure table as dict
    - handle lists and dicts as detachable child objects

--- speckle/serialization/base_object_serializer.py
+++ speckle/serialization/base_object_serializer.py
@@ -1,31 +1,37 @@
 import json
 import hashlib
 
-from typing import Tuple
+from typing import Any, Dict, List, Tuple
 from speckle.objects.base import Base
+from speckle.logging.exceptions import SerializationException
+
+PRIMITIVES = (int, float, str, bool)
 
 
-def hash_dict(obj: dict) -> str:
-    return hashlib.sha224(json.dumps(obj).encode()).hexdigest()[:32]
+def hash_obj(obj: Any) -> str:
+    return hashlib.sha256(json.dumps(obj).encode()).hexdigest()[:32]
 
 
 class BaseObjectSerializer:
-    detach_lineage: list = []
-    closure_table: dict = {}
-    traversed_objects: dict = {}
-    in_iterable: bool = False
+    detach_lineage: List[bool] = []  # tracks depth and whether or not to detach
+    children_helper: Dict = {}  # holds all the children in the root object
+    traversed_objects: Dict = {}  # all bases (root and its detached children)
+    closure_table: Dict = {}  # hash of each base and its children with min depth
+    in_iterable: bool = False  # tracks whether we are in a list or dict
 
     def __init__(self) -> None:
         pass
 
-    def to_json(self, obj: Base):
-        raise NotImplementedError
+    def to_json(self, base: Base):
+        self.detach_lineage = [True]
 
     def traverse_base(self, base: Base) -> Tuple[str, dict]:
         if not self.detach_lineage:
             self.detach_lineage.append(True)
         object_dict = {"id": ""}
+        children = []
         obj, props = base, base.get_member_names()
+
         while props:
             prop = props.pop(0)
             value = obj[prop]
@@ -38,44 +44,100 @@ class BaseObjectSerializer:
             if isinstance(value, Base):
                 if prop.startswith("@"):
                     self.detach_lineage.append(True)
-                    ref_hash, _ = self.traverse_base(value)
-                    object_dict[prop] = {
-                        "reference_id": ref_hash,
-                        "speckle_type": "reference",
-                    }
 
-                    if (
-                        ref_hash in self.closure_table
-                        and self.closure_table[ref_hash] < len(self.detach_lineage) + 1
-                    ):
-                        pass
-                    else:
-                        self.closure_table[ref_hash] = len(self.detach_lineage) + 1
+                    ref_hash, child_obj = self.traverse_base(value)
+                    children.append(ref_hash)
+
+                    object_dict[prop] = self.detach_helper(
+                        ref_hash=ref_hash, obj=child_obj
+                    )
+
                 else:
                     self.detach_lineage.append(False)
+
                     _, child_obj = self.traverse_base(value)
                     object_dict[prop] = child_obj
                 continue
 
-            # TODO: handle iterables (lists and dictionaries)
-            if isinstance(value, (list, dict)):
-                self.in_iterable = True
+            # all other cases
+            else:
+                if isinstance(value, (list, dict)):
+                    self.in_iterable = True
+
+                traversed = self.traverse_value(value)
+
+                if prop.startswith("@"):
+                    ref_hash = hash_obj(traversed)
+                    object_dict[prop] = self.detach_helper(
+                        ref_hash=ref_hash, obj=traversed
+                    )
+                    children.append(ref_hash)
+
+                else:
+                    object_dict[prop] = traversed
 
                 self.in_iterable = False
 
-            # TODO: primitive case, unknown object case
-            # all other cases
-            else:
-                object_dict[prop] = value
-
-        hash = hash_dict(object_dict)
+        hash = hash_obj(object_dict)
         object_dict["id"] = hash
 
         # write detached or root objects to transports
         if self.detach_lineage[-1]:
             self.traversed_objects[hash] = object_dict
 
-        if self.detach_lineage and not self.in_iterable:
+        if self.detach_lineage:
             self.detach_lineage.pop()
 
+        # add closures to object and to closure table
+        if children:
+            lineage_length = len(self.detach_lineage)
+            object_dict["__closure"] = self.closure_table[hash] = {
+                hash: minDepth - lineage_length if lineage_length > 0 else minDepth
+                for hash, minDepth in self.children_helper.items()
+            }
+
         return hash, object_dict
+
+    def traverse_value(self, obj: Any) -> Any:
+        if isinstance(obj, PRIMITIVES):
+            return obj
+
+        elif isinstance(obj, (list, set)):
+            return [self.traverse_value(o) for o in obj]
+
+        elif isinstance(obj, dict):
+            for k, v in obj.items():
+                if isinstance(v, PRIMITIVES):
+                    continue
+                else:
+                    obj[k] = self.traverse_value(v)
+            return obj
+
+        elif isinstance(obj, Base):
+            _, base_obj = self.traverse_base(obj)
+            return base_obj
+
+        else:
+            try:
+                return obj.dict()
+            except:
+                SerializationException(
+                    message=f"Failed to handle {type(obj)} in `BaseObjectSerializer.traverse_value`",
+                    object=obj,
+                )
+                return str(obj)
+
+    def detach_helper(self, ref_hash: str, obj: Any) -> Dict:
+        if ref_hash in self.children_helper and self.children_helper[ref_hash] < len(
+            self.detach_lineage
+        ):
+            pass
+        else:
+            self.children_helper[ref_hash] = len(self.detach_lineage)
+
+        self.traversed_objects[ref_hash] = obj
+
+        return {
+            "reference_id": ref_hash,
+            "speckle_type": "reference",
+        }
