7295689e1275efcfba23b5a8e1503ae9d680f961
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-11-27 18:31:33 +0000

    feat(operations): implement send

--- speckle/api/operations.py
+++ speckle/api/operations.py
@@ -1,38 +1,55 @@
-from speckle.transports.abstract_transport import AbstractTransport
+from typing import List
+from speckle.logging.exceptions import SpeckleException
 from speckle.objects.base import Base
+from speckle.transports.abstract_transport import AbstractTransport
+from speckle.serialization.base_object_serializer import BaseObjectSerializer
 
 
-class Operations:
-    def __init__(self) -> None:
+def send(
+    base: Base,
+    transports: List[AbstractTransport] = [],
+    use_default_cache: bool = True,
+):
+    """Sends an object via the provided transports. Defaults to the local cache.
+
+    Arguments:
+        obj {Base} -- the object you want to send
+        transports {list} -- where you want to send them
+        use_default_cache {bool} -- toggle for the default cache. If set to false, it will only send to the provided transports
+
+    Returns:
+        str -- the object id of the sent object
+    """
+    if not transports and not use_default_cache:
+        raise SpeckleException(
+            message="You need to provide at least one transport: cannot send with an empty transport list and no default cache"
+        )
+    if use_default_cache:
+        # TODO: finish sqlite transport and chuck it in here
         pass
 
-    def send(self, obj: Base, transports: list = [], use_default_cache: bool = True):
-        """Sends an object via the provided transports. Defaults to the local cache.
+    serializer = BaseObjectSerializer(write_transports=transports)
 
-        Arguments:
-            obj {Base} -- the object you want to send
-            transports {list} -- where you want to send them
-            use_default_cache {bool} -- toggle for the default cache. If set to false, it will only send to the provided transports
+    for t in transports:
+        t.begin_write()
+    hash, obj = serializer.write_json(base=base)
 
-        Returns:
-            str -- the object id of the sent object
-        """
-        raise NotImplementedError
+    return hash
 
-    def receive(
-        self,
-        obj_id: str,
-        remote_transport: AbstractTransport,
-        local_transport: AbstractTransport,
-    ) -> Base:
-        """Receives an object from a transport.
 
-        Arguments:
-            obj_id {str} -- the id of the object to receive
-            remote_transport {Transport} -- the transport to receive from
-            local_transport {Transport} -- the transport to send from
+def receive(
+    obj_id: str,
+    remote_transport: AbstractTransport,
+    local_transport: AbstractTransport,
+) -> Base:
+    """Receives an object from a transport.
 
-        Returns:
-            Base -- the base object
-        """
-        raise NotImplementedError
+    Arguments:
+        obj_id {str} -- the id of the object to receive
+        remote_transport {Transport} -- the transport to receive from
+        local_transport {Transport} -- the transport to send from
+
+    Returns:
+        Base -- the base object
+    """
+    raise NotImplementedError
