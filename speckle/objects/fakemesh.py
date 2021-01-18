2ca184538dd6098845e54c7fbcfb1fc94bf0cbd7
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-01-18 11:43:19 +0000

    docs(objects): update docstring & add example obj

---
+++ speckle/objects/fakemesh.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import List, Optional
+
+from .base import Base
+
+CHUNKABLE_PROPS = {
+    "vertices": 100,
+    "faces": 100,
+    "colors": 100,
+    "textureCoordinates": 100,
+    "test_bases": 10,
+}
+
+
+class FakeMesh(Base):
+    vertices: List[float] = None
+    faces: List[int] = None
+    colors: List[int] = None
+    textureCoordinates: List[float] = None
+    test_bases: List[Base] = None
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self._chunkable.update(CHUNKABLE_PROPS)
