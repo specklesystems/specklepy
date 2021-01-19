e14c42dfd10262736568a41355fa75019169dd18
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-01-19 11:04:34 +0000

    test(serialisation): they work, yo 🥳

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,57 +1,40 @@
 import json
-import random
-
-from gql.transport import transport
+import pytest
+from speckle.api import operations
+from speckle.transports.server import ServerTransport
 from speckle.transports.memory import MemoryTransport
 from speckle.serialization.base_object_serializer import BaseObjectSerializer
-from speckle.api import operations
-from speckle.objects.fakemesh import FakeMesh
-from speckle.objects.base import Base
-import pytest
 
 
-@pytest.fixture(scope="session")
-def mesh():
-    mesh = FakeMesh()
-    mesh.name = "my_mesh"
-    mesh.vertices = [random.uniform(0, 10) for _ in range(1, 210)]
-    mesh.faces = [i for i in range(1, 210)]
-    mesh["@(100)colours"] = [random.uniform(0, 10) for _ in range(1, 210)]
-    mesh["@()default_chunk"] = [random.uniform(0, 10) for _ in range(1, 210)]
-    mesh.test_bases = [Base(name=i) for i in range(1, 22)]
-    mesh["@detach"] = Base(name="detached base")
-    return mesh
+@pytest.mark.run(order=2)
+class TestSerialization:
+    def test_serialize(self, base):
+        serialized = operations.serialize(base)
+        deserialized = operations.deserialize(serialized)
 
+        assert base.get_id() == deserialized.get_id()
 
-@pytest.fixture(scope="session")
-def base():
-    base = Base()
-    base.name = "my_base"
-    base.vertices = [random.uniform(0, 10) for _ in range(1, 120)]
-    base.test_bases = [Base(name=i) for i in range(1, 22)]
-    base["@detach"] = Base(name="detached base")
-    return base
+    def test_chunking(self, mesh):
+        transport = MemoryTransport()
+        s = BaseObjectSerializer(write_transports=[transport], read_transport=transport)
+        _, serialized = s.write_json(mesh)
+        deserialized = s.read_json(serialized)
 
+        serialized_dict = json.loads(serialized)
 
-def test_serialize(base):
-    serialized = operations.serialize(base)
-    deserialized = operations.deserialize(serialized)
+        assert len(serialized_dict["vertices"]) == 3
+        assert len(serialized_dict["@(100)colours"]) == 3
+        assert len(serialized_dict["@()default_chunk"]) == 1
+        assert serialized_dict["vertices"][0]["speckle_type"] == "reference"
+        assert serialized_dict["@(100)colours"][0]["speckle_type"] == "reference"
+        assert serialized_dict["@()default_chunk"][0]["speckle_type"] == "reference"
+        assert mesh.get_id(True) == deserialized.get_id()
 
-    assert base.get_id() == deserialized.get_id()
+    def test_send_and_receive(self, client, sample_stream, mesh):
+        transport = ServerTransport(client=client, stream_id=sample_stream.id)
+        hash = operations.send(mesh, transports=[transport])
+        received = operations.receive(hash, remote_transport=transport)
 
+        assert mesh.get_id(True) == received.get_id()
 
-def test_chunking(mesh):
-    transport = MemoryTransport()
-    s = BaseObjectSerializer(write_transports=[transport], read_transport=transport)
-    _, serialized = s.write_json(mesh)
-    deserialized = s.read_json(serialized)
-
-    serialized_dict = json.loads(serialized)
-
-    assert len(serialized_dict["vertices"]) == 3
-    assert len(serialized_dict["@(100)colours"]) == 3
-    assert len(serialized_dict["@()default_chunk"]) == 1
-    assert serialized_dict["vertices"][0]["speckle_type"] == "reference"
-    assert serialized_dict["@(100)colours"][0]["speckle_type"] == "reference"
-    assert serialized_dict["@()default_chunk"][0]["speckle_type"] == "reference"
-    assert mesh.get_id(True) == deserialized.get_id()
+        mesh.id = hash  # populate with decomposed id for use in proceeding tests
