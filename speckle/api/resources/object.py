063191e961ba68bea0e334b7bb3b9de2449da1b4
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-01-19 15:56:38 +0000

    docs(object): update docstring

--- speckle/api/resources/object.py
+++ speckle/api/resources/object.py
@@ -54,10 +54,12 @@ class Resource(ResourceBase):
 
     def create(self, stream_id: str, objects: List[Dict]) -> str:
         """
-        Create a new object on a stream. To send a base object, you can prepare it by running it through the
-        `operations.serialize` function to get a valid (serialisable) object to send.
+        Not advised - generally, you want to use `operations.send()`.
 
-        NOTE: this does not create a commit - you can create one with `SpeckleClient.commit.create`.
+        Create a new object on a stream. To send a base object, you can prepare it by running it through the
+        `BaseObjectSerializer.traverse_base()` function to get a valid (serialisable) object to send.
+
+        NOTE: this does not create a commit - you can create one with `SpeckleClient.commit.create`. Dynamic fields will be located in the 'data' dict of the received `Base` object
 
         Arguments:
             stream_id {str} -- the id of the stream you want to send the object to
