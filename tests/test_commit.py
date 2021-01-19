8b9f2db176494607b1cb2f13a96351d3fbe40387
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-01-19 11:02:11 +0000

    test: refactor into classes to use ordering

--- tests/test_commit.py
+++ tests/test_commit.py
@@ -1,43 +1,59 @@
-from tests.test_stream import stream
-from speckle.api.models import Commit
 import pytest
+from speckle.api.models import Commit, Stream
 
 
-@pytest.fixture(scope="module")
-def commit():
-    return Commit(message="a fun little test commit")
+@pytest.mark.run(order=3)
+class TestCommit:
+    @pytest.fixture(scope="module")
+    def commit(self):
+        return Commit(message="a fun little test commit")
 
+    @pytest.fixture(scope="module")
+    def updated_commit(
+        self,
+    ):
+        return Commit(message="a fun little updated commit")
 
-@pytest.fixture(scope="module")
-def updated_commit():
-    return Commit(message="a fun little updated commit")
+    @pytest.fixture(scope="module")
+    def stream(self, client):
+        stream = Stream(
+            name="a sample stream for testing",
+            description="a stream created for testing",
+            isPublic=True,
+        )
+        stream.id = client.stream.create(
+            stream.name, stream.description, stream.isPublic
+        )
+        return stream
 
+    def test_commit_create(self, client, stream, mesh, commit):
+        commit.id = client.commit.create(
+            stream_id=stream.id, object_id=mesh.id, message=commit.message
+        )
 
-@pytest.fixture(scope="module")
-def stream_id(client):
-    return client.stream.create("testing commits")
+        assert isinstance(commit.id, str)
 
+    def test_commit_get(self, client, stream, mesh, commit):
+        fetched_commit = client.commit.get(stream_id=stream.id, commit_id=commit.id)
 
-def test_create(client, stream_id, commit):
-    commit.id = client.commit.create(
-        stream_id=stream_id, object_id="object123", message=commit.message
-    )
+        assert fetched_commit.message == commit.message
+        assert fetched_commit.referencedObject == mesh.id
 
-    assert isinstance(commit.id, str)
+    def test_commit_update(self, client, stream, commit, updated_commit):
+        updated = client.commit.update(
+            stream_id=stream.id, commit_id=commit.id, message=updated_commit.message
+        )
 
+        fetched_commit = client.commit.get(stream_id=stream.id, commit_id=commit.id)
 
-def test_get(client, stream_id, commit):
-    fetched_commit = client.commit.get(stream_id=stream_id, commit_id=commit.id)
+        assert updated is True
+        assert fetched_commit.message == updated_commit.message
 
-    assert fetched_commit.message == commit.message
+    def test_commit_delete(self, client, stream, mesh):
+        commit_id = client.commit.create(
+            stream_id=stream.id, object_id=mesh.id, message="a great commit to delete"
+        )
 
+        deleted = client.commit.delete(stream_id=stream.id, commit_id=commit_id)
 
-def test_update(client, stream_id, commit, updated_commit):
-    updated = client.commit.update(
-        stream_id=stream_id, commit_id=commit.id, message=updated_commit.message
-    )
-
-    fetched_commit = client.commit.get(stream_id=stream_id, commit_id=commit.id)
-
-    assert updated == True
-    assert fetched_commit.message == updated_commit.message
+        assert deleted is True
--- tests/test_stream.py
+++ tests/test_stream.py
@@ -1,80 +1,76 @@
-from speckle.logging.exceptions import GraphQLException
 import pytest
 from speckle.api.models import Stream
+from speckle.logging.exceptions import GraphQLException
 
 
-@pytest.fixture(scope="module")
-def stream():
-    return Stream(
-        name="a wonderful stream",
-        description="a stream created for testing",
-        isPublic=True,
-    )
+@pytest.mark.run(order=1)
+class TestStream:
+    @pytest.fixture(scope="session")
+    def stream(self):
+        return Stream(
+            name="a wonderful stream",
+            description="a stream created for testing",
+            isPublic=True,
+        )
 
+    @pytest.fixture(scope="module")
+    def updated_stream(
+        self,
+    ):
+        return Stream(
+            name="a wonderful updated stream",
+            description="an updated stream description for testing",
+            isPublic=False,
+        )
 
-@pytest.fixture(scope="module")
-def updated_stream():
-    return Stream(
-        name="a wonderful updated stream",
-        description="an updated stream description for testing",
-        isPublic=False,
-    )
+    def test_stream_create(self, client, stream, updated_stream):
+        stream.id = updated_stream.id = client.stream.create(
+            name=stream.name,
+            description=stream.description,
+            is_public=stream.isPublic,
+        )
 
+        assert isinstance(stream.id, str)
 
-def test_create(client, stream):
-    stream_id = client.stream.create(
-        name=stream.name,
-        description=stream.description,
-        is_public=stream.isPublic,
-    )
-    stream.id = stream_id
+    def test_stream_get(self, client, stream):
+        fetched_stream = client.stream.get(stream.id)
 
-    assert isinstance(stream_id, str)
+        assert fetched_stream.name == stream.name
+        assert fetched_stream.description == stream.description
+        assert fetched_stream.isPublic == stream.isPublic
 
+    def test_stream_update(self, client, updated_stream):
+        updated = client.stream.update(
+            id=updated_stream.id,
+            name=updated_stream.name,
+            description=updated_stream.description,
+            is_public=updated_stream.isPublic,
+        )
+        fetched_stream = client.stream.get(updated_stream.id)
 
-def test_get(client, stream):
-    stream = client.stream.get(stream.id)
+        assert updated is True
+        assert fetched_stream.name == updated_stream.name
+        assert fetched_stream.description == updated_stream.description
+        assert fetched_stream.isPublic == updated_stream.isPublic
 
-    assert stream.name == stream.name
-    assert stream.description == stream.description
-    assert stream.isPublic == stream.isPublic
+    def test_stream_list(self, client):
+        client.stream.create(name="a second wonderful stream")
+        client.stream.create(name="a third fantastic stream")
 
+        streams = client.stream.list()
 
-def test_update(client, stream, updated_stream):
-    updated = client.stream.update(
-        id=stream.id,
-        name=updated_stream.name,
-        description=updated_stream.description,
-        is_public=updated_stream.isPublic,
-    )
-    fetched_stream = client.stream.get(stream.id)
+        assert len(streams) >= 3
 
-    assert updated == True
-    assert fetched_stream.name == updated_stream.name
-    assert fetched_stream.description == updated_stream.description
-    assert fetched_stream.isPublic == updated_stream.isPublic
+    def test_stream_search(self, client, updated_stream):
+        search_results = client.stream.search(updated_stream.name)
 
+        assert len(search_results) == 1
+        assert search_results[0].name == updated_stream.name
 
-def test_list(client):
-    client.stream.create(name="a second wonderful stream")
-    client.stream.create(name="a third fantastic stream")
+    def test_stream_delete(self, client, stream):
+        deleted = client.stream.delete(stream.id)
 
-    streams = client.stream.list()
+        stream_get = client.stream.get(stream.id)
 
-    assert len(streams) >= 3
-
-
-def test_search(client, updated_stream):
-    search_results = client.stream.search(updated_stream.name)
-
-    assert len(search_results) == 1
-    assert search_results[0].name == updated_stream.name
-
-
-def test_delete(client, stream):
-    deleted = client.stream.delete(stream.id)
-
-    stream_get = client.stream.get(stream.id)
-
-    assert deleted == True
-    assert isinstance(stream_get, GraphQLException)
+        assert deleted is True
+        assert isinstance(stream_get, GraphQLException)
