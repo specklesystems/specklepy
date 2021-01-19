062808c568aadf70a6774c44ecb62f663b83ff1a
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-01-19 11:03:03 +0000

    test: move fixtures to conf for sharing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,11 @@
 import uuid
+import random
 import pytest
 import requests
+from speckle.api.models import Stream
 from speckle.api.client import SpeckleClient
+from speckle.objects.base import Base
+from speckle.objects.fakemesh import FakeMesh
 
 
 @pytest.fixture(scope="session")
@@ -45,3 +49,37 @@ def client(host, seed_user):
     client = SpeckleClient(host=host, use_ssl=False)
     client.authenticate(seed_user["token"])
     return client
+
+
+@pytest.fixture(scope="session")
+def sample_stream(client):
+    stream = Stream(
+        name="a sample stream for testing",
+        description="a stream created for testing",
+        isPublic=True,
+    )
+    stream.id = client.stream.create(stream.name, stream.description, stream.isPublic)
+    return stream
+
+
+@pytest.fixture(scope="session")
+def mesh():
+    mesh = FakeMesh()
+    mesh.name = "my_mesh"
+    mesh.vertices = [random.uniform(0, 10) for _ in range(1, 210)]
+    mesh.faces = [i for i in range(1, 210)]
+    mesh["@(100)colours"] = [random.uniform(0, 10) for _ in range(1, 210)]
+    mesh["@()default_chunk"] = [random.uniform(0, 10) for _ in range(1, 210)]
+    mesh.test_bases = [Base(name=i) for i in range(1, 22)]
+    mesh["@detach"] = Base(name="detached base")
+    return mesh
+
+
+@pytest.fixture(scope="session")
+def base():
+    base = Base()
+    base.name = "my_base"
+    base.vertices = [random.uniform(0, 10) for _ in range(1, 120)]
+    base.test_bases = [Base(name=i) for i in range(1, 22)]
+    base["@detach"] = Base(name="detached base")
+    return base
