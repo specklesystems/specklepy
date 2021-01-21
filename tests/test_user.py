0110b5b11a377d45fe7e870f3a77eaff6d38e958
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-01-21 11:07:08 +0000

    test(user): get and search

---
+++ tests/test_user.py
@@ -0,0 +1,32 @@
+from speckle.api.models import User
+import pytest
+
+
+@pytest.mark.run(order=1)
+class TestUser:
+    def test_user_get_self(self, client, user_dict):
+        fetched_user = client.user.get()
+
+        assert isinstance(fetched_user, User)
+        assert fetched_user.name == user_dict["name"]
+        assert fetched_user.email == user_dict["email"]
+
+        user_dict["id"] = fetched_user.id
+
+    def test_user_search(self, client, second_user_dict):
+        search_results = client.user.search(search_query=second_user_dict["name"][:5])
+
+        assert isinstance(search_results, list)
+        assert isinstance(search_results[0], User)
+        assert search_results[0].name == second_user_dict["name"]
+
+        second_user_dict["id"] = search_results[0].id
+
+    def test_user_get(self, client, second_user_dict):
+        fetched_user = client.user.get(id=second_user_dict["id"])
+
+        assert isinstance(fetched_user, User)
+        assert fetched_user.name == second_user_dict["name"]
+        assert fetched_user.email == second_user_dict["email"]
+
+        second_user_dict["id"] = fetched_user.id
