6ab9252398160085f7a24aa06dbdf61910c9c136
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-01-21 15:33:06 +0000

    test(serialisation): both sqlite & server receive

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,4 +1,5 @@
 import json
+from speckle.objects.base import Base
 import pytest
 from speckle.api import operations
 from speckle.transports.server import ServerTransport
@@ -33,8 +34,18 @@ class TestSerialization:
     def test_send_and_receive(self, client, sample_stream, mesh):
         transport = ServerTransport(client=client, stream_id=sample_stream.id)
         hash = operations.send(mesh, transports=[transport])
-        received = operations.receive(hash)
+        # use a fresh memory transport to force receiving from remote
+        received = operations.receive(
+            hash, remote_transport=transport, local_transport=MemoryTransport()
+        )
 
+        assert isinstance(received, Base)
         assert mesh.get_id(True) == received.get_id()
 
         mesh.id = hash  # populate with decomposed id for use in proceeding tests
+
+    def test_receive_local(self, client, mesh):
+        received = operations.receive(mesh.id)  # defaults to SQLiteTransport
+
+        assert isinstance(received, Base)
+        assert mesh.get_id(True) == received.get_id()
