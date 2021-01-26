236d53f78acd33c011471fd3432f0376432c8a29
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-01-26 11:49:36 +0000

    feat(obj): finish point example

--- speckle/objects/point.py
+++ speckle/objects/point.py
@@ -1,10 +1,9 @@
 from typing import List
-from pydantic import BaseModel
 from speckle.objects.base import Base
 
 
 class Point(Base):
-    value: List[float] = [0, 0, 0]
+    value: List[int or float] = [0, 0, 0]
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(value: {self.value}, id: {self.id}, speckle_type: {self.speckle_type})"
@@ -23,3 +22,18 @@ class Point(Base):
     @property
     def z(self):
         return self.value[2]
+
+    @x.setter
+    def x(self, value: int or float):
+        if isinstance(value, (int, float)):
+            self.value[0] = value
+
+    @y.setter
+    def y(self, value: int or float):
+        if isinstance(value, (int, float)):
+            self.value[1] = value
+
+    @z.setter
+    def z(self, value: int or float):
+        if isinstance(value, (int, float)):
+            self.value[2] = value
