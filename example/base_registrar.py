5f35da3be944063cfac30bad064dda9463fa4bc8
Gergő Jedlicska <gergo@jedlicska.com>
2021-02-11 21:43:28 +0100

    style(black config): added explicit black config to pyproject.toml

--- example/base_registrar.py
+++ example/base_registrar.py
@@ -6,9 +6,10 @@ from typing import ClassVar, Dict, Type
 class ExampleBase(BaseModel):
     foo: int
 
-    _registry : ClassVar[Dict[str, Type["ExampleBase"]]] = {}
+    speckle_type: ClassVar[str] = "base"
+    _registry: ClassVar[Dict[str, Type["ExampleBase"]]] = {}
 
-    def __init_subclass__(cls, speckle_type:str, **kwargs):
+    def __init_subclass__(cls, speckle_type: str, **kwargs):
         # this requires some validation, there is nothing blocking identical keys...
         cls.speckle_type = speckle_type
         ExampleBase._registry[speckle_type] = cls
@@ -20,18 +21,20 @@ class ExampleSub(ExampleBase, speckle_type="sub"):
 
 
 class SpeckleSub(ExampleSub, speckle_type="speckle_sub"):
-    magic : str
+    magic: str
 
 
 # # uncommenting this raises an error, for missing speckle_type definition
 # # this automatically enforces mandatory type definition for everyone
 # class FaultySub(ExampleBase):
-    # pass
+#     pass
 
 
 if __name__ == "__main__":
 
-    for k,v in ExampleBase._registry.items():
+    for k, v in ExampleBase._registry.items():
         print(f"Speckle type {k}, mapped to {v.__name__}")
 
-    print(ExampleBase._registry["speckle_sub"](foo=123, magic="trick", bar="baric").json())    
+    print(
+        ExampleBase._registry["speckle_sub"](foo=123, magic="trick", bar="baric").json()
+    )
