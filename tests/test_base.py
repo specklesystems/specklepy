7f2c26eb6b9950c5c221d2a543f533ce170b523e
Gergő Jedlicska <gergo@jedlicska.com>
2021-02-11 22:04:20 +0100

    refactor(test_base.py): refactor base object invalid prop tests
    Utilizing a few pytest features to make invalid prop tests a bit sleeker

--- tests/test_base.py
+++ tests/test_base.py
@@ -2,22 +2,18 @@ import pytest
 from speckle.objects.base import Base
 
 
-class TestBase:
-    def test_empty_prop_names(self):
-        base = Base()
-        with pytest.raises(ValueError):
-            base[""] = "empty"
-        with pytest.raises(ValueError):
-            base["@"] = "empty"
+@pytest.mark.parametrize(
+    "invalid_prop_name",
+    [
+        (""),
+        ("@"),
+        ("@@wow"),
+        ("this.is.bad"),
+        ("super/bad"),
+    ],
+)
+def test_empty_prop_names(invalid_prop_name: str) -> None:
+    base = Base()
+    with pytest.raises(ValueError):
+        base[invalid_prop_name] = "🐛️"
 
-    def test_invalid_prop_names(self):
-        base = Base()
-
-        with pytest.raises(ValueError):
-            base["@@wow"] = "bad"
-
-        with pytest.raises(ValueError):
-            base["this.is.bad"] = "also bad"
-
-        with pytest.raises(ValueError):
-            base["super/bad"] = "no no no"
