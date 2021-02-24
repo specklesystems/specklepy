9a0207ba099b8f75da6977247c78ebd9a431c1b5
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-02-24 18:34:40 +0000

    feat(obj): update point class
    remove value attribute

--- speckle/objects/point.py
+++ speckle/objects/point.py
@@ -1,39 +1,20 @@
-from typing import List
+from typing import Any
 from speckle.objects.base import Base
 
 
 class Point(Base, speckle_type="Objects.Geometry.Point"):
-    value: List[int or float] = [0, 0, 0]
+    x: float = 0
+    y: float = 0
+    z: float = 0
+
+    def __init__(self, x: float = 0, y: float = 0, z: float = 0, **data: Any) -> None:
+        super().__init__(**data)
+        self.x = x
+        self.y = y
+        self.z = z
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(value: {self.value}, id: {self.id}, speckle_type: {self.speckle_type})"
+        return f"{self.__class__.__name__}(x: {self.x}, y: {self.y}, z: {self.z}, id: {self.id}, speckle_type: {self.speckle_type})"
 
     def __str__(self) -> str:
         return self.__repr__()
-
-    @property
-    def x(self):
-        return self.value[0]
-
-    @property
-    def y(self):
-        return self.value[1]
-
-    @property
-    def z(self):
-        return self.value[2]
-
-    @x.setter
-    def x(self, value: int or float):
-        if isinstance(value, (int, float)):
-            self.value[0] = value
-
-    @y.setter
-    def y(self, value: int or float):
-        if isinstance(value, (int, float)):
-            self.value[1] = value
-
-    @z.setter
-    def z(self, value: int or float):
-        if isinstance(value, (int, float)):
-            self.value[2] = value
