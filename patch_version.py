73703f6237b28c928b5fb81bea27f790c3c1381a
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-08-10 15:38:02 +0100

    ci: patch version with git tag

---
+++ patch_version.py
@@ -0,0 +1,31 @@
+import re
+import sys
+
+
+def patch(tag):
+    print(f"Patching version: {tag}")
+
+    with open("pyproject.toml", "r") as f:
+        lines = f.readlines()
+
+        if "version" not in lines[2]:
+            raise Exception(f"Invalid pyproject.toml. Could not patch version.")
+
+        lines[2] = f'version = "{tag}"\n'
+        with open("pyproject.toml", "w") as file:
+            file.writelines(lines)
+
+
+def main():
+    if len(sys.argv) < 2:
+        return
+
+    tag = sys.argv[1]
+    if not re.match(r"[0-9]+(\.[0-9]+)*$", tag):
+        raise ValueError(f"Invalid tag provided: {tag}")
+
+    patch(tag)
+
+
+if __name__ == "__main__":
+    main()
