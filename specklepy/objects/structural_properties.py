ffa4f29200fdb9ef0a269bc5bf6398b836f932fc
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-12-10 11:18:24 +0000

    fix(structural): correct import path for Axis

--- specklepy/objects/structural_properties.py
+++ specklepy/objects/structural_properties.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Any, List, Optional
 
-from structural_geometry import Axis
+from .structural_geometry import Axis
 
 from .base import Base
 from .encoding import CurveArray, CurveTypeEncoding, ObjectArray
