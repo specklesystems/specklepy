40a691b098b955523b330a92718dd97af1f4a0e8
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-12-10 11:16:38 +0000

    style(structural): formatting pass
    @Reynold-Chan please use Black for code formatting to keep the style
    consistent with the rest of the repo. you can set your editor to
    automatically format on save

--- specklepy/objects/structural_analysis.py
+++ specklepy/objects/structural_analysis.py
@@ -10,42 +10,46 @@ from .structural_properties import *
 
 STRUCTURAL_ANALYSIS = "Objects.Structural.Analysis."
 
-class ModelUnits(Base, speckle_type = STRUCTURAL_ANALYSIS + "ModelUnits"):
-    length : str = None
+
+class ModelUnits(Base, speckle_type=STRUCTURAL_ANALYSIS + "ModelUnits"):
+    length: str = None
     sections: str = None
     displacements: str = None
-    stress : str = None
-    force : str = None
-    mass : str = None
-    time : str = None
-    temperature : str = None
-    velocity : str = None
-    acceleration : str = None
+    stress: str = None
+    force: str = None
+    mass: str = None
+    time: str = None
+    temperature: str = None
+    velocity: str = None
+    acceleration: str = None
     energy: str = None
-    angle : str = None
-    strain : str =None
+    angle: str = None
+    strain: str = None
 
-class ModelSetttings(Base, speckle_type = STRUCTURAL_ANALYSIS + "ModelSettings"):
-    modelUnits : ModelUnits = None
-    steelCode : str = None
-    concreteCode : str = None
-    coincidenceTolerance : float = 0.0
 
-class ModelInfo(Base, speckle_type = STRUCTURAL_ANALYSIS + "ModelInfo"):
-    name : str = None
-    description : str = None
-    projectNumber : str = None
-    projectName : str = None
-    settings : ModelSetttings = None
-    initials : str = None
-    application : str = None
+class ModelSetttings(Base, speckle_type=STRUCTURAL_ANALYSIS + "ModelSettings"):
+    modelUnits: ModelUnits = None
+    steelCode: str = None
+    concreteCode: str = None
+    coincidenceTolerance: float = 0.0
 
-class Model(Base, speckle_type =  STRUCTURAL_ANALYSIS + "Model"):
-    specs : ModelInfo = None
-    nodes : List = None
-    elements : List = None
-    loads : List = None
-    restraints : List = None
-    properties : List = None
-    materials : List = None
-    layerDescription : str = None
+
+class ModelInfo(Base, speckle_type=STRUCTURAL_ANALYSIS + "ModelInfo"):
+    name: str = None
+    description: str = None
+    projectNumber: str = None
+    projectName: str = None
+    settings: ModelSetttings = None
+    initials: str = None
+    application: str = None
+
+
+class Model(Base, speckle_type=STRUCTURAL_ANALYSIS + "Model"):
+    specs: ModelInfo = None
+    nodes: List = None
+    elements: List = None
+    loads: List = None
+    restraints: List = None
+    properties: List = None
+    materials: List = None
+    layerDescription: str = None
--- specklepy/objects/structural_material.py
+++ specklepy/objects/structural_material.py
@@ -11,16 +11,17 @@ STRUCTURAL_MATERIALS = "Objects.Structural.Materials"
 class MaterialType(int, Enum):
     Concrete = 0
     Steel = 1
-    Timber = 2 
+    Timber = 2
     Aluminium = 3
     Masonry = 4
     FRP = 5
     Glass = 6
     Fabric = 7
-    Rebar = 8 
+    Rebar = 8
     Tendon = 9
     ColdFormed = 10
     Other = 11
+
 
 class Material(Base, speckle_type=STRUCTURAL_MATERIALS):
     name: str = None
@@ -32,27 +33,29 @@ class Material(Base, speckle_type=STRUCTURAL_MATERIALS):
     elasticModulus: float = 0.0
     poissonsRatio: float = 0.0
     shearModulus: float = 0.0
-    density: float = 0.0 
+    density: float = 0.0
     thermalExpansivity: float = 0.0
     dampingRatio: float = 0.0
     cost: float = 0.0
-    materialSafetyFactor: float = 0.0  
+    materialSafetyFactor: float = 0.0
 
-class Concrete(Material, speckle_type = STRUCTURAL_MATERIALS + ".Concrete"):
+
+class Concrete(Material, speckle_type=STRUCTURAL_MATERIALS + ".Concrete"):
     compressiveStrength: float = 0.0
     tensileStrength: float = 0.0
     flexuralStrength: float = 0.0
     maxCompressiveStrength: float = 0.0
-    maxTensileStrength: float = 0.0 
+    maxTensileStrength: float = 0.0
     maxAggregateSize: float = 0.0
     lightweight: bool = None
 
-class Steel(Material, speckle_type = STRUCTURAL_MATERIALS + ".Steel"):
+
+class Steel(Material, speckle_type=STRUCTURAL_MATERIALS + ".Steel"):
     yieldStrength: float = 0.0
-    ultimateStrength: float = 0.0 
-    maxStrain: float = 0.0 
-    strainHardeningModulus: float =0.0
+    ultimateStrength: float = 0.0
+    maxStrain: float = 0.0
+    strainHardeningModulus: float = 0.0
 
-class Timber(Material, speckle_type = STRUCTURAL_MATERIALS + ".Timber"):
+
+class Timber(Material, speckle_type=STRUCTURAL_MATERIALS + ".Timber"):
     species: str = None
-
