0b14660115c728724b623735d390788145f34dae
Knuttatutta <knut.tjensvoll@hotmail.com>
2023-02-01 10:16:13 +0100

    Add AxisType, fix type in Axis

--- src/specklepy/objects/structural/axis.py
+++ src/specklepy/objects/structural/axis.py
@@ -1,10 +1,17 @@
 from typing import Optional
+from enum import Enum
 
 from specklepy.objects.base import Base
 from specklepy.objects.geometry import Plane
 
 
+class AxisType(int, Enum):
+    Cartesian = 0
+    Cylindrical = 1
+    Spherical = 2
+
+
 class Axis(Base, speckle_type="Objects.Structural.Geometry.Axis"):
     name: Optional[str] = None
-    axisType: Optional[str] = None
+    axisType: Optional[AxisType] = None
     plane: Optional[Plane] = None
