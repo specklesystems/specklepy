deb8ad50c56351897d75acd9329378425049c854
Gergő Jedlicska <gergo@jedlicska.com>
2023-12-11 17:34:26 +0100

    fix: client certificate verification

--- src/specklepy/api/client.py
+++ src/specklepy/api/client.py
@@ -50,11 +50,16 @@ class SpeckleClient(CoreSpeckleClient):
     DEFAULT_HOST = "speckle.xyz"
     USE_SSL = True
 
-    def __init__(self, host: str = DEFAULT_HOST, use_ssl: bool = USE_SSL, verify_certificate: bool = True) -> None:
+    def __init__(
+        self,
+        host: str = DEFAULT_HOST,
+        use_ssl: bool = USE_SSL,
+        verify_certificate: bool = True,
+    ) -> None:
         super().__init__(
             host=host,
             use_ssl=use_ssl,
-            verify_certificate: bool = VERIFY_CERTIFICATE,
+            verify_certificate=verify_certificate,
         )
         self.account = Account()
 
