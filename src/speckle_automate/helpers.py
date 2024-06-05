2e35fb9e5c233dc7cd888ba60174d166308c421f
Charles Driesler <chuck@speckle.systems>
2024-06-05 16:38:49 +0100

    create helpers for testing functions

--- src/speckle_automate/helpers.py
+++ src/speckle_automate/helpers.py
@@ -3,50 +3,94 @@ import secrets
 import string
 
 from gql import gql
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from speckle_automate.schema import AutomationRunData, TestAutomationRunData
 from specklepy.api.client import SpeckleClient
 
 
-def register_new_automation(
-    speckle_client: SpeckleClient,
-    project_id: str,
-    model_id: str,
-    automation_id: str,
-    automation_name: str,
-    automation_revision_id: str,
-) -> bool:
-    """Register a new automation in the speckle server."""
+class TestAutomationEnvironment(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_prefix="speckle_",
+        extra="ignore",
+    )
+
+    token: str = Field()
+    server_url: str = Field()
+    project_id: str = Field()
+    automation_id: str = Field()
+
+
+def get_test_automation_environment() -> TestAutomationEnvironment:
+    """Get known environment variables from local `.env` file"""
+
+    return TestAutomationEnvironment().model_dump()
+
+
+def create_test_automation_run(
+    speckle_client: SpeckleClient, project_id: str, test_automation_id: str
+) -> TestAutomationRunData:
+    """Create test run to report local test results to"""
+
     query = gql(
         """
-        mutation CreateAutomation(
-            $projectId: String!
-            $modelId: String!
-            $automationName: String!
-            $automationId: String!
-            $automationRevisionId: String!
+        mutation CreateTestRun(
+            $projectId: ID!,
+            $automationId: ID!
         ) {
-                automationMutations {
-                    create(
-                        input: {
-                            projectId: $projectId
-                            modelId: $modelId
-                            automationName: $automationName
-                            automationId: $automationId
-                            automationRevisionId: $automationRevisionId
+            projectMutations {
+                automationMutations(projectId: $projectId) {
+                    createTestAutomationRun(automationId: $automationId) {
+                        automationRunId
+                        functionRunId
+                        triggers {
+                            payload {
+                                modelId
+                                versionId
+                            }
+                            triggerType
                         }
-                    )
+                    }
                 }
             }
+        }
         """
     )
-    params = {
-        "projectId": project_id,
-        "modelId": model_id,
-        "automationName": automation_name,
-        "automationId": automation_id,
-        "automationRevisionId": automation_revision_id,
-    }
-    return speckle_client.httpclient.execute(query, params)
+
+    params = {"automationId": test_automation_id, "projectId": project_id}
+
+    result = speckle_client.httpclient.execute(query, params)
+
+    return (
+        result.get("projectMutations")
+        .get("automationMutations")
+        .get("createTestAutomationRun")
+    )
+
+
+def create_test_automation_run_data(
+    speckle_client: SpeckleClient,
+    speckle_server_url: str,
+    project_id: str,
+    test_automation_id: str,
+) -> AutomationRunData:
+    """Create automation run data for a new run for a given test automation"""
+
+    test_automation_run_data = create_test_automation_run(
+        speckle_client, project_id, test_automation_id
+    )
+
+    return AutomationRunData(
+        project_id=project_id,
+        speckle_server_url=speckle_server_url,
+        automation_id=test_automation_id,
+        automation_run_id=test_automation_run_data.get("automation_run_id"),
+        function_run_id=test_automation_run_data.get("function_run_id"),
+        triggers=test_automation_run_data.get("triggers"),
+    )
 
 
 def crypto_random_string(length: int) -> str:
