be8fae3b1ccf1457ca1fc5243b41e081a442e558
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2024-10-30 15:55:15 +0000

    removed unused subscription functions

--- src/specklepy/core/api/resources/subscription_resource.py
+++ src/specklepy/core/api/resources/subscription_resource.py
@@ -48,7 +48,6 @@ class SubscriptionResource(ResourceBase):
             name=NAME,
         )
 
-    @check_wsclient
     async def user_projects_updated(
         self, callback: Callable[[UserProjectsUpdatedMessage], None]
     ) -> None:
@@ -82,7 +81,6 @@ class SubscriptionResource(ResourceBase):
             callback=lambda d: callback(d.data),
         )
 
-    @check_wsclient
     async def project_models_updated(
         self,
         callback: Callable[[ProjectModelsUpdatedMessage], None],
@@ -127,7 +125,6 @@ class SubscriptionResource(ResourceBase):
             callback=lambda d: callback(d.data),
         )
 
-    @check_wsclient
     async def project_updated(
         self,
         callback: Callable[[ProjectUpdatedMessage], None],
@@ -165,7 +162,6 @@ class SubscriptionResource(ResourceBase):
             callback=lambda d: callback(d.data),
         )
 
-    @check_wsclient
     async def project_versions_updated(
         self,
         callback: Callable[[ProjectVersionsUpdatedMessage], None],
@@ -209,112 +205,7 @@ class SubscriptionResource(ResourceBase):
             callback=lambda d: callback(d.data),
         )
 
-    @deprecated(reason=FE1_DEPRECATION_REASON, version=FE1_DEPRECATION_VERSION)
     @check_wsclient
-    async def stream_added(self, callback: Optional[Callable] = None):
-        """Subscribes to new stream added event for your profile.
-        Use this to display an up-to-date list of streams.
-
-        Arguments:
-            callback {Callable[Stream]} -- a function that takes the updated stream
-            as an argument and executes each time a stream is added
-
-        Returns:
-            Stream -- the update stream
-        """
-        query = gql(
-            """
-            subscription { userStreamAdded }
-            """
-        )
-        return await self.subscribe(
-            query=query, callback=callback, return_type="userStreamAdded", schema=Stream
-        )
-
-    @deprecated(reason=FE1_DEPRECATION_REASON, version=FE1_DEPRECATION_VERSION)
-    @check_wsclient
-    async def stream_updated(self, id: str, callback: Optional[Callable] = None):
-        """
-        Subscribes to stream updated event.
-        Use this in clients/components that pertain only to this stream.
-
-        Arguments:
-            id {str} -- the stream id of the stream to subscribe to
-            callback {Callable[Stream]}
-            -- a function that takes the updated stream
-            as an argument and executes each time the stream is updated
-
-        Returns:
-            Stream -- the update stream
-        """
-        query = gql(
-            """
-            subscription Update($id: String!) { streamUpdated(streamId: $id) }
-            """
-        )
-        params = {"id": id}
-
-        return await self.subscribe(
-            query=query,
-            params=params,
-            callback=callback,
-            return_type="streamUpdated",
-            schema=Stream,
-        )
-
-    @deprecated(reason=FE1_DEPRECATION_REASON, version=FE1_DEPRECATION_VERSION)
-    @check_wsclient
-    async def stream_removed(self, callback: Optional[Callable] = None):
-        """Subscribes to stream removed event for your profile.
-        Use this to display an up-to-date list of streams for your profile.
-        NOTE: If someone revokes your permissions on a stream,
-        this subscription will be triggered with an extra value of revokedBy
-        in the payload.
-
-        Arguments:
-            callback {Callable[Dict]}
-            -- a function that takes the returned dict as an argument
-            and executes each time a stream is removed
-
-        Returns:
-            dict -- dict containing 'id' of stream removed and optionally 'revokedBy'
-        """
-        query = gql(
-            """
-            subscription { userStreamRemoved }
-            """
-        )
-
-        return await self.subscribe(
-            query=query,
-            callback=callback,
-            return_type="userStreamRemoved",
-            parse_response=False,
-        )
-
-    @deprecated(reason=FE1_DEPRECATION_REASON, version=FE1_DEPRECATION_VERSION)
-    @check_wsclient
-    async def subscribe(
-        self,
-        query: DocumentNode,
-        params: Optional[Dict] = None,
-        callback: Optional[Callable] = None,
-        return_type: Optional[Union[str, List]] = None,
-        schema=None,
-        parse_response: bool = True,
-    ):
-        # if self.client.transport.websocket is None:
-        # TODO: add multiple subs to the same ws connection
-        async with self.client as session:
-            async for res in session.subscribe(query, variable_values=params):
-                res = self._step_into_response(response=res, return_type=return_type)
-                if parse_response:
-                    res = self._parse_response(response=res, schema=schema)
-                if callback is not None:
-                    callback(res)
-                else:
-                    return res
-
     async def subscribe_2(
         self,
         response_type: Type[TEventArgs],
