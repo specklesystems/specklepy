cf9abb48e8cdaf5bd5c1e8d7f1f687a57e2a7899
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-04-30 18:58:48 +0300

    WIP on v3-dev

--- src/specklepy/core/api/inputs/user_inputs.py
+++ src/specklepy/core/api/inputs/user_inputs.py
@@ -13,8 +13,8 @@ class UserUpdateInput(GraphQLBaseModel):
 class UserProjectsFilter(GraphQLBaseModel):
     search: Optional[str] = None
     only_with_roles: Optional[Sequence[str]] = None
-    workspaceId: Optional[str] = None
-    personalOnly: Optional[bool] = None
+    workspace_id: Optional[str] = None
+    personal_only: Optional[bool] = None
     include_implicit_access: Optional[bool] = None
 
 
