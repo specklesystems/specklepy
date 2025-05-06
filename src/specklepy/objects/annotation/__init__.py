95de5cbb30e5ff48b8a51bbcbbe1599645c3853a
KatKatKateryna <89912278+KatKatKateryna@users.noreply.github.com>
2025-05-06 10:12:29 +0100

    Introducing Text class (#419)
    * add text class and tests
    
    * formatting
    
    * fix default values
    
    * comments
    
    * comment
    
    * sort imports
    
    * import alignments
    
    * compare properties, not Base objects
    
    * revert irrelevant changes
    
    * tests
    
    * use correct fixture
    
    * fix tests property

---
+++ src/specklepy/objects/annotation/__init__.py
@@ -0,0 +1,8 @@
+from .text import AlignmentHorizontal, AlignmentVertical, Text
+
+# re-export them at the geometry package level
+__all__ = [
+    "Text",
+    "AlignmentHorizontal",
+    "AlignmentVertical",
+]
