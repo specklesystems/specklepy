ebeffd0404e1894cb80aac25fcfcd37cddb74c15
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-07-18 11:41:32 +0100

    Added connector feed utility functions

---
+++ tests/integration/test_connector_versions.py
@@ -0,0 +1,30 @@
+import pytest
+from httpx import HTTPStatusError
+
+from specklepy.core.api.connector_versions import (
+    ConnectorVersion,
+    ConnectorVersions,
+    get_connector_versions,
+    get_latest_version,
+)
+
+# NOTE: the tests in this file are testing against the live releases.speckle.dev server
+# url defined in get_connector_versions.
+
+
+def test_connector_versions():
+    res = get_connector_versions("blender")
+
+    assert isinstance(res, ConnectorVersions)
+    assert res.versions  # Assuming the feed is not empty
+
+
+def test_get_latest_version_throws_no_slug():
+    with pytest.raises(HTTPStatusError):
+        get_latest_version("non-existent-connector!", True)
+
+
+def test_get_latest_version():
+    res = get_latest_version("blender", False)
+
+    assert isinstance(res, ConnectorVersion)
