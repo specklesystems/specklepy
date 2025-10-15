2e0639470e06e48962122844d29c519a2858c1e2
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-10-15 21:26:42 +0100

    correct the doc string

--- src/specklepy/transports/server/retry_policy.py
+++ src/specklepy/transports/server/retry_policy.py
@@ -4,16 +4,6 @@ from urllib3 import Retry
 
 
 def setup_session(auth_token: str | None) -> requests.Session:
-    session = requests.Session()
-    retry_policy = Retry(
-        total=3,
-        read=3,
-        connect=3,
-        backoff_factor=0.5,
-        status_forcelist=(500, 502, 503, 504, 408, 429),
-        allowed_methods=["HEAD", "GET", "OPTIONS", "POST", "PUT", "DELETE"],
-        raise_on_status=False,
-    )
     """
     Sets up a requests.Session with a basic retry policy
     to retry on all the usual retryable status codes, with a back off policy:
@@ -24,6 +14,17 @@ def setup_session(auth_token: str | None) -> requests.Session:
     Also sets "Accept": "text/plain" header (because this is what ServerTransport needs)
     and (if a auth_token is provided) the Authorization header
     """
+
+    session = requests.Session()
+    retry_policy = Retry(
+        total=3,
+        read=3,
+        connect=3,
+        backoff_factor=0.5,
+        status_forcelist=(500, 502, 503, 504, 408, 429),
+        allowed_methods=["HEAD", "GET", "OPTIONS", "POST", "PUT", "DELETE"],
+        raise_on_status=False,
+    )
 
     adapter = HTTPAdapter(max_retries=retry_policy)
     session.mount("https://", adapter)
