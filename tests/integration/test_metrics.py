6074c790b35557ea36ad540e951bfcb53c9a4d23
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-12-09 14:58:09 +0000

    clean up tests

--- tests/integration/test_metrics.py
+++ tests/integration/test_metrics.py
@@ -1,55 +1,76 @@
 from typing import Any, Callable
 
+import pytest
 from pytest_httpserver import HTTPServer
+from requests import HTTPError
 from werkzeug import Request, Response
 
 from specklepy.core.api.client import SpeckleClient
 from specklepy.logging import metrics
 
+PATH = "/"
 
-def test_metrics_track(httpserver: HTTPServer, client: SpeckleClient):
-    extra_assertion: Callable[[Any], bool]
 
-    def handler(request: Request) -> Response:
+def assert_common_properties(payload: Any) -> None:
+    assert payload["event"] == "SDK Action"
+    assert payload["properties"]["token"] == "acd87c5a50b56df91a795e999812a3a4"
+    assert payload["properties"]["type"] == "action"
+    assert payload["properties"]["server_id"]
+    assert payload["properties"]["distinct_id"]
+    assert payload["properties"]["hostApp"] == "python"
+    assert payload["properties"]["hostAppVersion"]
+    assert payload["properties"]["core_version"]
+
+
+def handler(extra_check: Callable[[Any], bool]) -> Callable[[Request], Response]:
+    def inner(request: Request) -> Response:
         json = request.get_json()
-
         payload = json[0]
-        assert payload["event"] == "SDK Action"
-        assert payload["properties"]["token"] == "acd87c5a50b56df91a795e999812a3a4"
-        assert payload["properties"]["type"] == "action"
-        assert payload["properties"]["server_id"]
-        assert payload["properties"]["distinct_id"]
-        assert payload["properties"]["hostApp"] == "python"
-        assert payload["properties"]["hostAppVersion"]
-        assert payload["properties"]["core_version"]
-
-        assert extra_assertion(payload)
+        assert_common_properties(payload)
+        assert extra_check(payload)
         return Response("", 200)
 
-    httpserver.expect_request("/", "post").respond_with_handler(handler)
-    with ScopedMetricsWrapper(httpserver.url_for("/")) as _:
+    return inner
+
+
+def test_metrics_track(httpserver: HTTPServer, client: SpeckleClient):
+    with ScopedMetricsWrapper(httpserver.url_for(PATH)) as _:
         # Test No email
-        extra_assertion = lambda payload: "email" not in payload["properties"]  # noqa: E731
+        httpserver.expect_oneshot_request(PATH, "post").respond_with_handler(
+            handler(lambda payload: "email" not in payload["properties"])
+        )
         metrics.track("SDK Action", client.account, None, True, False)
 
         # Test With email
-        extra_assertion = (  # noqa: E731
-            lambda payload: payload["properties"]["email"]
-            == client.account.userInfo.email
+        httpserver.expect_oneshot_request(PATH, "post").respond_with_handler(
+            handler(
+                lambda payload: payload["properties"]["email"]
+                == client.account.userInfo.email
+            )
         )
         metrics.track("SDK Action", client.account, None, True, True)
 
         # Test With custom value
-        extra_assertion = (  # noqa: E731
-            lambda payload: payload["properties"]["myCustomProp"] == "myCustomValue"
+        httpserver.expect_oneshot_request(PATH, "post").respond_with_handler(
+            handler(
+                lambda payload: payload["properties"]["myCustomProp"] == "myCustomValue"
+            )
         )
         metrics.track(
             "SDK Action", client.account, {"myCustomProp": "myCustomValue"}, True, True
         )
 
 
-def test_metrics_errors(httpserver: HTTPServer, client: SpeckleClient):
-    pass
+def test_metrics_errors(httpserver: HTTPServer):
+    with ScopedMetricsWrapper(httpserver.url_for(PATH)) as _:
+        httpserver.expect_oneshot_request(PATH, "post").respond_with_data("", 400)
+
+        # Expect send_sync == true to mean mean it will raise
+        with pytest.raises(HTTPError):
+            metrics.track("SDK Action", send_sync=True)
+
+        # Expect send_sync == false to mean mean it won't
+        metrics.track("SDK Action")
 
 
 class ScopedMetricsWrapper:
